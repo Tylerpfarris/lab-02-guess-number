67b94ace5a8bccbd3558643ffe948e1009d0d6d3
katrinkajb@gmail.com <katrinkajb@gmail.com>
2021-01-14 16:10:46 -0800

    refactor and bug fixes

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-import { compareNumbersFunc, checkGuessesLeftFunc } from './utils.js';
+import { compareNumbersFunc, checkGuessesLeftFunc, messageStringFunc, makeRandomNumberFunc } from './utils.js';
 
 const makeGuessButton = document.getElementById('makeGuess');
 const userGuessInput = document.getElementById('user-Guess');
 const resetButton = document.getElementById('reset');
 
+let randomNumber = makeRandomNumberFunc();
+
 let guessesRemainSpan = document.getElementById('guesses-Remain');
 let usersGuessesRemaining = 4;
-const randomNumber = Math.ceil(Math.random() * 20);
 
-//guess button
 makeGuessButton.addEventListener('click', () => {
-  
     --usersGuessesRemaining;
+    
     guessesRemainSpan.textContent = usersGuessesRemaining;
     checkGuessesLeftFunc(guessesRemainSpan);
 
-    if (usersGuessesRemaining === 0) {
-        makeGuessButton.disabled = true;
-    }
-    compareNumbersFunc((Number(userGuessInput.value)), randomNumber);
+    if (usersGuessesRemaining === 0) makeGuessButton.disabled = true;
+    
+    const compReturn = compareNumbersFunc((Number(userGuessInput.value)), randomNumber);
+    messageStringFunc(compReturn);
 });
 
 //reset button 
@@ -28,5 +28,5 @@ resetButton.addEventListener('click', () => {
     guessesRemainSpan.textContent = usersGuessesRemaining;
     usersGuessesRemaining = 4;
     makeGuessButton.disabled = false;
-
+    randomNumber = makeRandomNumberFunc();
 });        
--- utils.js
+++ utils.js
@@ -1,32 +1,38 @@
-
 const tooHighOrLowSpan = document.getElementById('toHighOrLow');
 
+export function makeRandomNumberFunc() {
+    return Math.ceil(Math.random() * 20);
+}
+
 export function compareNumbersFunc(guess, correctNumber) {
-    //message strings
+    if (guess > correctNumber) return 1;
+    if (guess < correctNumber) return -1;
+    return 0;
+}
+
+export function messageStringFunc(compareReturn) {
     const tooHighString = `thats too high`;
     const tooLowString = `thats too low`;
     const youGotItString = `you nailed it!!!`;
-    
-    if (guess > correctNumber) {
-        tooHighOrLowSpan.textContent = tooHighString;
-        return 1;
-    } else if (guess < correctNumber) {
-        tooHighOrLowSpan.textContent = tooLowString;
-        return -1;
-    } else {
-        tooHighOrLowSpan.textContent = youGotItString;
-        return 0;
-        
-    }
 
+    if (compareReturn === 1) {
+        tooHighOrLowSpan.textContent = tooHighString;
+    }
+    else if (compareReturn === -1) {
+        tooHighOrLowSpan.textContent = tooLowString;
+    }
+    else if (compareReturn === 0) {
+        tooHighOrLowSpan.textContent = youGotItString;
+    }
 }
+
     //checking if user has any guesses left
 
 export function checkGuessesLeftFunc(usersGuesses) {
     const noGuessesLeft = `Luck wasn't on your side today`;
+
     if (usersGuesses === 0) {
-        tooHighOrLowSpan.textContent = noGuessesLeft;
-        return 1; 
+        tooHighOrLowSpan.textContent = noGuessesLeft; 
     }
 }
 
